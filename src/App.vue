fffe1c19a608ba36e4d2e18d6412eb495f15923a
Adam Grady <Adam.Grady@live.com>
2020-09-18 15:01:25 -0500

    adding electron

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+        </div>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+
+@Component({
+    name: "App"
+})
+export default class App extends Vue {
+    
+}
+</script>
 
 <style lang="scss">
 #app {
