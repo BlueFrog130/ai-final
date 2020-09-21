ec30ea7b2a4144c32a4e8060f14b6e0496a18031
Adam Grady <AGrady@edcohis.com>
2020-09-20 22:59:54 -0500

    updating packages

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-        </div>
+        <Card :card="card" hide-by-default />
+        <button @click="onClick">Add Card</button>
+        <button @click="calc">Calc Score</button>
         <router-view/>
     </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator"
+import { Deck } from "@/models/deck"
+import { Card } from "@/models/card"
+import CardComponent from "@/components/Card.vue"
+import { Score } from "@/models/score"
 
 @Component({
-    name: "App"
+    name: "App",
+    components: {
+        "Card": CardComponent
+    }
 })
 export default class App extends Vue {
-    
+    private readonly deck = new Deck();
+
+    private card: Card | null = null;
+
+    private cards: Card[] = [];
+
+    private created() {
+        // nothing
+    }
+
+    onClick() {
+        this.cards = this.cards.concat(this.deck.deal(1));
+        console.log(this.cards.map(c => c.value));
+    }
+
+    calc() {
+        console.log(Score.total(this.cards));
+    }
 }
 </script>
 
@@ -22,21 +46,6 @@ export default class App extends Vue {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
